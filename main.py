8ad6e8eb2240f5f86f68b7b81c06266f3e091299
alex <alecksey1308@gmail.com>
2021-03-27 17:07:08 +0300

    print(items, sum, order_num)

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 items = os.getenv('ITEMS')
 sum = os.getenv('SUM')
 order_num = os.getenv('ORDER_NUM')
 
+print(items)
+print(sum)
+print(order_num)
